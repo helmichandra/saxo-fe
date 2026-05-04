206c674b76a6f3913a7b7ce9f6459d53b668039c
helmi.santoso <helmi.santoso@dataon.com>
2026-05-04 14:18:33 +0700

    chore: update next.js version

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,22 @@
-const nextConfig = {
-  assetPrefix: ".",
-  async rewrites() {
-    return [
-      {
-        source: "/binance-api/:path*",
-        destination: "https://api.binance.com/:path*",
-      },
-    ];
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  assetPrefix : "."
+};
+
+module.exports = {
   images: {
     domains: ["s2.coinmarketcap.com"],
   },
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  typescript: {
+  typescript:{
     ignoreBuildErrors: true,
-  },
+  }
 };
 
 export default nextConfig;
