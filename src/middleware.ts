9b798e08ca29cc7d133f31ce2819b08fdad33ab6
helmi.santoso <helmychandra17@gmail.com>
2026-05-12 19:41:34 +0700

    downgrade

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,50 +1,82 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function middleware(request: any) {
+  const sessionId =
+    request.cookies.get("sessionId")?.value;
 
-  const sessionId = request.cookies.get("sessionId")?.value;
-  const roleId = request.cookies.get("roleId")?.value;
+  const roleId =
+    request.cookies.get("roleId")?.value;
 
   const url = request.nextUrl.clone();
 
+  // redirect recovery
   if (url.pathname === "/auth/recovery") {
     url.pathname = "/auth/signin";
+
     return NextResponse.redirect(url);
   }
 
+  // kalau sudah login jangan balik auth
   if (
     sessionId &&
-    (url.pathname === "/auth/signin" ||
+    (
+      url.pathname === "/auth/signin" ||
       url.pathname === "/auth/signup" ||
-      url.pathname === "/auth/code-register")
+      url.pathname === "/auth/code-register"
+    )
   ) {
     url.pathname = "/dashboard";
+
     return NextResponse.redirect(url);
   }
 
-  if (!sessionId && url.pathname.startsWith("/dashboard")) {
+  // belum login
+  if (
+    !sessionId &&
+    url.pathname.startsWith("/dashboard")
+  ) {
     url.pathname = "/auth/signin";
+
     return NextResponse.redirect(url);
   }
 
-  if ((url.pathname === "/dashboard/markets" || url.pathname === "/dashboard") && (roleId === "777" || roleId === "555")) {
+  // admin redirect
+  if (
+    (
+      url.pathname === "/dashboard/markets" ||
+      url.pathname === "/dashboard"
+    ) &&
+    (
+      roleId === "777" ||
+      roleId === "555"
+    )
+  ) {
     url.pathname = "/dashboard/users";
+
     return NextResponse.redirect(url);
   }
 
-  if ((url.pathname === "/dashboard" || url.pathname === "/dashboard") && (roleId === "1" || roleId === "")) {
-    url.pathname = "/dashboard/user-view";
+  // user redirect
+  if (
+    url.pathname === "/dashboard" &&
+    (
+      roleId === "1" ||
+      roleId === ""
+    )
+  ) {
+    url.pathname =
+      "/dashboard/user-view";
+
     return NextResponse.redirect(url);
   }
-
 
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    "/dashboard/:path*", 
-    "/auth/signin",      
+    "/dashboard/:path*",
+    "/auth/signin",
     "/auth/signup",
     "/auth/code-register",
     "/auth/recovery",
